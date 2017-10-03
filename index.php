740bf0b38d319f1a79bba09da2ae65192cda8026
Pierre Schuvey <pierre.schuveye2n@gmail.com>
2017-10-03 15:38:16 +0200

    optimisation du code de l'exercice 6 PHP de la prairie

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
-
 <?php
+/*Ceci est mon ancien code
+
 $langages = array("HTML","CSS","Javascript","PHP");
 sort($langages);
 foreach ($langages as $key => $val) {
-    echo 'langages =  '.$val.'<br />';
+    echo $val.'<br />';
 }
 
 function fusion($langages)
@@ -12,5 +13,36 @@ function fusion($langages)
       echo $val;
       }
 }
-echo fusion($langages);
+echo fusion($langages);*/
+
+//Déclaration du tableau $language
+$language = array("HTML","CSS","Javascript","PHP");
+//Déclaration de la fonction alphabetic() qui permet de trier $language par ordre alphabétique
+function alphabetic($code){
+  //vérification que le paramètre est un tableau
+  if(is_array($code)==true){
+    //tri du tableau
+    sort($code);
+  }
+  return $code;
+}
+echo 'Le tableau langage trié :<br />';
+//on appelle la fonction alphabetic() et on stocke le résultat dans le tableau $sortedArray
+$sortedArray = alphabetic($language);
+//On parcours le tableau avec la boucle foreach pour afficher son contenu
+foreach($sortedArray as $value){
+  echo $value . '<br />';
+}
+
+//Déclaration de la fonction concat() qui permet de renvoyer le contenu d'un tableau concaténé
+function concat($code){
+  //vérification que le paramètre est bien un tableau
+  if(is_array($code)==true){
+    //Rassemble les éléments du tableau dns la chaîne $string
+    $string = implode(' ',$code);
+  }
+  return $string;
+}
+echo '<br />Le tableau langage concaténé :<br />';
+echo concat($language);
 ?>
